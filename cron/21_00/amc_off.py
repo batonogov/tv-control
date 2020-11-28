3470030aa133c6ce5e5e9554d05ffbab8f6dfd6c
Fedor Batonogov <batonogov@outlook.com>
2020-11-28 07:14:20 +0300

    Create amc_off.py

---
+++ cron/21_00/amc_off.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.insert(0, "/usr/local/cron/tv_control")
+from AMC import *
+
+
+amc_list = '/usr/local/cron/tv_list/amc_list'
+port = 2525
+
+with open(amc_list) as amc:
+    for line in amc:
+        print(line)
+        print(send_command(power_off, line, port))
