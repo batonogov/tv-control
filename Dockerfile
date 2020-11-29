e0354d100dc16061d7a8b930c4bc86daaa62d4fb
Fedor Batonogov <batonogov@outlook.com>
2020-11-29 08:15:05 +0300

    Улучшение работы

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM batonogov/cron
 
+RUN apt install iputils-ping -y
+
 COPY scheduler/* /usr/local/cron/
--- cron/21_00/amc_off.py
+++ cron/21_00/amc_off.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 sys.path.insert(0, "/usr/local/cron/tv_control")
 from AMC import *
 
@@ -6,7 +6,15 @@ from AMC import *
 amc_list = '/usr/local/cron/tv_list/amc_list'
 port = 2525
 
+tv_list = []
+
 with open(amc_list) as amc:
     for line in amc:
-        print(line)
-        print(send_command(power_off, line, port))
+        tv_list.append(line)
+
+for tv in tv_list:
+    response = os.system('ping -c 1 ' + tv)
+    if response == 0:
+        send_command(power_off, tv, port)
+    else:
+        print('Устройство', tv, 'недоступно.')
